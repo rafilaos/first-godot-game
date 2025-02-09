53ca54b81b1b98bd7c827e80003f0a608bd1af08
rafilaos <rafilaos@gmx.at>
2025-02-09 16:57:00 +0100

    .

--- first-game/player/player.gd
+++ first-game/player/player.gd
@@ -1,18 +1,23 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 const acceleration = 50
-var speed = 50
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-func get_input():
-	var input_direction = Input.get_vector("move player left ", "move player right","move player up","move player down")
-	velocity = input_direction * speed
-	
+var direction = Vector2(0 , 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float):
-	get_input()
-	move_and_slide()
+	if Input.is_action_just_pressed("move player down"):
+		direction = Vector2(0 , 50)
+		apply_central_force(direction * acceleration)
+		
+	if Input.is_action_just_pressed("move player up"):
+		direction = Vector2(0 , -50)
+		apply_central_force(direction * acceleration)
+		
+	if Input.is_action_just_pressed("move player left "):
+		direction = Vector2(-50 , 0)
+		apply_central_force(direction * acceleration)
+		
+	if Input.is_action_just_pressed("move player right"):
+		direction = Vector2(50 , 0)
+		apply_central_force(direction * acceleration)
+	
--- first-game/snake/snake.gd
+++ first-game/snake/snake.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
+@export var numBodyParts = 7
+@export var distanceBetweenBodyPart = 30
 var bodyPart = preload("res://snake/snake body part.tscn")
-var numBodyParts = 7
 var bodyPartPosition = 0
 
 func _ready():
 	for n in numBodyParts:
-		# "generating" the body parts and setting the distance
+		# "generating" the body parts 
 		var newBodyPart  = bodyPart.instantiate()
+		# setting the position for the next body part
 		newBodyPart.position += Vector2(0, bodyPartPosition)
-		add_child(newBodyPart)
+		add_sibling.call_deferred(newBodyPart)
 		# increases the distance between the body parts for the next one
-		bodyPartPosition = bodyPartPosition + 10
+		bodyPartPosition = bodyPartPosition + distanceBetweenBodyPart
+		
+func _physics_process(delta: float) -> void:
+	pass
